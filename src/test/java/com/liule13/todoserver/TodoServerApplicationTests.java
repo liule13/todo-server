b1fb5b39e5a31b15b495001ab5d55dd4a75028a8
liule13 <LEO.LIU@OOCL.COM>
2025-09-16 10:36:29 +0800

    feat: init commit

---
+++ src/test/java/com/liule13/todoserver/TodoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liule13.todoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
